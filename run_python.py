1a179c18c19f0c4b55246b7f4834ef8e81bb7eea
Ashwin Narayan <ashwinn@mit.edu>
2018-12-02 22:34:57 -0500

    testing non-log modification of tails

--- run_python.py
+++ run_python.py
@@ -2,22 +2,22 @@ import numpy as np
 import bhtsne
 from sklearn.decomposition import PCA
 
-data = np.loadtxt('example_data/pollen.txt',delimiter=',').T
+# data = np.loadtxt('../example_data/pollen.txt',delimiter=',').T
 
-data = np.log(1+data)
+# data = np.log(1+data)
 
-data = data - np.mean(data, axis=1, keepdims=True)
-data = data/(np.sum(data**2, axis=1, keepdims=True))**.5
+# data = data - np.mean(data, axis=1, keepdims=True)
+# data = data/(np.sum(data**2, axis=1, keepdims=True))**.5
 
 
-pca = PCA(n_components=50)
+# pca = PCA(n_components=50)
 
-pc_data = pca.fit_transform(data)
+# pc_data = pca.fit_transform(data)
 
 pc_data = np.loadtxt('gaussian_density_drastic.txt').T
 
 print(pc_data.shape)
 
-embedded = bhtsne.run_bh_tsne(pc_data, initial_dims=pc_data.shape[1], theta=0., verbose=True)
+embedded = bhtsne.run_bh_tsne(pc_data, initial_dims=pc_data.shape[1], theta=0., verbose=True, perplexity=80)
 
 np.savetxt('bh_out.txt', embedded)
